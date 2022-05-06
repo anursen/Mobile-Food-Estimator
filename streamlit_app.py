bbca9183eae98f3b5dca0792126f7af906ffba68
anursen <96952461+anursen@users.noreply.github.com>
2022-05-05 22:03:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,37 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import cv2
+from skimage import io
 
+
+st.file_selector 
+
+import streamlit as st
+import os
+
+filename = st.text_input('Enter a file path:')
+try:
+    with open(filename) as input:
+        st.text(input.read())
+except FileNotFoundError:
+    st.error('File not found.')
+    
+
+
+
+cake_url = "https://storage.googleapis.com/tfhub-visualizers/google/aiy/vision/classifier/food_V1/1/image_1.jpg"
+labelmap_url = "https://www.gstatic.com/aihub/tfhub/labelmaps/aiy_food_V1_labelmap.csv"
+input_shape = (224, 224)
+
+image = np.asarray(io.imread(cake_url), dtype="float")
+image = cv2.resize(image, dsize=input_shape, interpolation=cv2.INTER_CUBIC)
+# Scale values to [0, 1].
+image = image / image.max()
+# The model expects an input of (?, 224, 224, 3).
+images = np.expand_dims(image, 0)
+# This assumes you're using TF2.
+output = m(images)
+predicted_index = output.numpy().argmax()
+classes = list(pd.read_csv(labelmap_url)["name"])
+print("Prediction: ", classes[predicted_index])
