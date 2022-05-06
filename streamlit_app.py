293095de4d9c2adbceb9b6158eed1b1208b6199a
anursen <96952461+anursen@users.noreply.github.com>
2022-05-05 22:19:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,6 @@ import pandas as pd
 import cv2
 from skimage import io
 
-
-st.file_selector 
-
 import streamlit as st
 import os
 
@@ -20,7 +17,7 @@ except FileNotFoundError:
 
 
 
-cake_url = "https://storage.googleapis.com/tfhub-visualizers/google/aiy/vision/classifier/food_V1/1/image_1.jpg"
+cake_url = filename
 labelmap_url = "https://www.gstatic.com/aihub/tfhub/labelmaps/aiy_food_V1_labelmap.csv"
 input_shape = (224, 224)
 
@@ -34,4 +31,4 @@ images = np.expand_dims(image, 0)
 output = m(images)
 predicted_index = output.numpy().argmax()
 classes = list(pd.read_csv(labelmap_url)["name"])
-print("Prediction: ", classes[predicted_index])
+st.write("Prediction: ", classes[predicted_index])
